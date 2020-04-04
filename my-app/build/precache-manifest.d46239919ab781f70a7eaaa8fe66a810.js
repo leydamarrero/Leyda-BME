cb5233c3b48806657eafe4ea781d181774539424
leydamarrero <leyda.marrero@ufl.edu>
2020-04-04 20:47:00 +0000

    hey

---
+++ my-app/build/precache-manifest.d46239919ab781f70a7eaaa8fe66a810.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c866d2ee6bf031612d2f968c8d37c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d954c0ce40023576491d",
+    "url": "/static/css/main.49478846.chunk.css"
+  },
+  {
+    "revision": "cc5b60f530ec9ef2959d",
+    "url": "/static/js/2.3dd64d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3dd64d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d954c0ce40023576491d",
+    "url": "/static/js/main.b3af06f9.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src= "./michaelscott.jpg" className="App-logo" alt="logo" />
+        <p>
+          Leyda Marrero-Morales
+        </p>
+        <a
+          className="App-link"
+          href="www.linkedin.com/in/leyda-marrero-032884164"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Take a look at my LinkedIn
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
